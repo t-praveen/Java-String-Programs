d1d11c4647d3c69c4ba950d3fa1159de450f81c7
T Praveen <62693938+t-praveen@users.noreply.github.com>
2021-05-15 12:58:06 +0530

    Add files via upload

---
+++ JavaStringProgram10.java
@@ -0,0 +1,20 @@
+package strings;
+
+import java.util.Scanner;
+
+public class JavaStringProgram10 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string : ");
+		String str = sc.nextLine();
+		System.out.print("Enter another String : ");
+		String s = sc.nextLine();
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println("Comparing "+str+" and "+sb+" : "+str.contentEquals(sb));
+		System.out.println("Comparing "+s+" and "+sb+" : "+s.contentEquals(sb));
+		sc.close();
+	}
+
+}
